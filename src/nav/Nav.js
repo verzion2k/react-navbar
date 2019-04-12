cd98ea655045302b540cf33ebc6aca1bd844f130
verzion2k <kovacs.zsoltzx@gmail.com>
2019-04-12 14:49:11 +0200

    Added fontAwesome

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './scss/Nav.scss';
+import logo from './icons/logo.png';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
 
-class Nav extends Component {
+library.add(faHamburger);
+
+export default class Nav extends Component {
 	render() {
-		return;
+		return (
+			<main className="nav">
+				<div className="nav__left">
+					<img src={logo} alt="logo" className="nav__logo" />
+				</div>
+				<div className="nav__main">
+					<h1 className="nav__title">Székesfehérvári Református Egyházközség</h1>
+				</div>
+				<div className="nav__right">
+					<FontAwesomeIcon icon="hamburger" className="nav__collapse__button" />
+				</div>
+			</main>
+		);
 	}
 }
-
-export default Nav;
